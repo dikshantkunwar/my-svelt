99945fe02a6da17e57bcd64c6a5c2fe55edbfbef
Dikshant Kunwar <dikshantkunwar@gmail.com>
2022-02-27 19:39:45 -0500

    Added more examples

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,31 @@
 <script>
+	import Nested from './Nested.svelte';
+	import Info from './Info.svelte';
+	import Cats from './Cats.svelte';
+
+	const pkg = {
+		name: 'svelte',
+		version: 3,
+		speed: 'blazing',
+		website: 'https://svelte.dev'
+	};
+
+	let user = { loggedIn: false };
+
 	export let name, count = 0;
+	let numbers = [1, 2, 3, 4];
+
+	$: if (count >= 10) {
+		alert('count is dangerously high!');
+		count = 9;
+	}
+
+	$: sum = numbers.reduce((t,n) => t + n, 0);
+
+	function addNumber() {
+		// numbers.push(numbers.length + 1);
+		numbers = [...numbers, numbers.length + 1];
+	}
 
 	function incrementCount() {
 		count += 1;
@@ -12,16 +38,37 @@
 	function resetCount() {
 		count = 0;
 	}
+
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
 </script>
 
 <main>
+	{#if user.loggedIn}
+		<button on:click={toggle}>Log out</button>
+	{:else}
+		<button on:click={toggle}>Log in</button>
+	{/if}
+
+	<br>
+	<p>User is {user.loggedIn ? 'ONLINE :)': 'OFFLINE :('}</p>
 	<button on:click={incrementCount}>
 		Clicked {count} {count === 1 ? 'time': 'times'}
 	</button>
 	<button on:click={decrementCount}> - </button>
 	<button on:click={resetCount}> RESET </button>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<p>{numbers.join(' + ')} = {sum}</p>
+	<button on:click={addNumber}>Add a number</button>
+
+	<Nested answer={55} />
+	<Nested />
+
+	<Cats />
+
+	<Info {...pkg} />
 </main>
 
 <style>
---
+++ src/Cats.svelte
@@ -0,0 +1,18 @@
+<script>
+  let cats = [
+    { id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+		{ id: 'z_AbfPXTKms', name: 'Maru' },
+		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+  ];
+</script>
+
+<div>
+  <h1>The Famous Cats of Internet</h1>
+  <ul>
+    {#each cats as cat}
+      <li><a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
+        {cat.name}
+      </a></li>
+    {/each}
+  </ul>
+</div>
